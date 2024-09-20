3e1f8ce9702dbcb38367c86f5c23acc3671c3ceb
Balandala <balandala05@gmail.com>
2024-09-20 07:44:50 +0500

    Раскостов Task12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -1,5 +1,7 @@
 package com.example.task12;
 
+import java.util.Objects;
+
 public class Task12Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -12,7 +14,17 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
+        if (!Objects.isNull(arr) && arr.length > 0) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                int min = i;
+                for (int j = i + 1; j < arr.length; j++)
+                    if (arr[j] < arr[min])
+                        min = j;
 
+                int buffer = arr[min];
+                arr[min] = arr[i];
+                arr[i] = buffer;
+            }
+        }
+    }
 }
